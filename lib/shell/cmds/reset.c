e778f66b1810f98220622a5be9320c1639d6da70
b-man <mckenzba@gmail.com>
2015-03-13 01:17:05 -0400

    Add missing copyright notice

--- lib/shell/cmds/reset.c
+++ lib/shell/cmds/reset.c
@@ -1,17 +1,17 @@
 /*
- * Copyright 2014, Brian McKenzie. <mckenzba@gmail.com>
+ * Copyright 2015, Brian McKenzie. <mckenzba@gmail.com>
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  *   Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  *   Redistributions in binary form must reproduce the above copyright notice, this
  *   list of conditions and the following disclaimer in the documentation and/or
  *   other materials provided with the distribution.
- * 
+ *
  *   If you are going to use this software in any form that does not involve
  *   releasing the source to this project or improving it, let me know beforehand.
  *
